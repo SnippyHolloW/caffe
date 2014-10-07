47c25b0570bd7b962d3214e6347f23a55f8e8279
syhw <gabriel.synnaeve@gmail.com>
2014-10-07 20:36:33 +0200

    anaconda+cuda file to source

---
+++ ANACONDA.sh
@@ -0,0 +1,3 @@
+export PATH=/Users/gabrielsynnaeve/anaconda/bin:/Applications/MATLAB_R2013a.app/bin:$PATH
+export PYTHONPATH=/Users/gabrielsynnaeve/labs/selective_search_ijcv_with_python::$PYTHONPATH
+export DYLD_FALLBACK_LIBRARY_PATH=$CUDA_ROOT/lib:$HOME/anaconda/lib:/usr/local/lib:/usr/lib
